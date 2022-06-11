4e930f882401b8eaf70150b74011ad97fa4a4388
JMEChristian Digital <jmechristian@gmail.com>
2022-06-11 18:09:05 -0400

    Form Setup

--- components/AddPlace.js
+++ components/AddPlace.js
@@ -1,7 +1,91 @@
-import React from 'react';
+import {
+  Box,
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Input,
+  FormHelperText,
+  Drawer,
+  DrawerContent,
+  DrawerOverlay,
+  DrawerCloseButton,
+  DrawerHeader,
+  DrawerBody,
+  Select,
+  Spacer,
+  Textarea,
+  useDisclosure,
+} from '@chakra-ui/react';
+import { ArrowForwardIcon } from '@chakra-ui/icons';
 
 const AddPlace = () => {
-  return <div>AddPlace</div>;
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <>
+      <Drawer isOpen={true} size={'md'}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader>Add New Project</DrawerHeader>
+          <DrawerBody>
+            <Box height={'100%'}>
+              <Flex direction='column'>
+                <FormControl>
+                  <Flex pb={'2'}>
+                    <Box>
+                      <FormLabel htmlFor='lng'>Longitude</FormLabel>
+                      <Input id='lng' type='lng' disabled={true} />
+                      <FormHelperText>Required</FormHelperText>
+                    </Box>
+                    <Spacer />
+                    <Box>
+                      <FormLabel htmlFor='lat'>Latitude</FormLabel>
+                      <Input id='lat' type='lat' disabled={true} />
+                      <FormHelperText>Required</FormHelperText>
+                    </Box>
+                  </Flex>
+                  <Box py={'2'}>
+                    <FormLabel htmlFor='name'>Project Name</FormLabel>
+                    <Input id='name' type='name' />
+                    <FormHelperText>Required</FormHelperText>
+                  </Box>
+                  <Box py={'2'}>
+                    <FormLabel htmlFor='dept'>Department</FormLabel>
+                    <Select placeholder='Select Department' id='dept'>
+                      <option value='option1'>Architecture</option>
+                      <option value='option2'>Commercial Interiors</option>
+                      <option value='option3'>AkRes</option>
+                      <option value='option3'>Branding &amp; Marketing</option>
+                    </Select>
+                    <FormHelperText>Required</FormHelperText>
+                  </Box>
+                  <Box py={'2'}>
+                    <FormLabel htmlFor='address'>Address</FormLabel>
+                    <Input id='address' type='address' />
+                  </Box>
+                  <Box py={'2'}>
+                    <FormLabel htmlFor='name'>
+                      Project Description (Optional)
+                    </FormLabel>
+                    <Textarea placeholder='Add optional project description' />
+                  </Box>
+                </FormControl>
+                <Button
+                  rightIcon={<ArrowForwardIcon />}
+                  colorScheme='teal'
+                  my={'4'}
+                  size={'lg'}
+                >
+                  Add to Map
+                </Button>
+              </Flex>
+            </Box>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+    </>
+  );
 };
 
 export default AddPlace;
--- components/ProjectMap.js
+++ components/ProjectMap.js
@@ -14,8 +14,10 @@ import {
   TagLeftIcon,
   TagLabel,
   Text,
+  Drawer,
 } from '@chakra-ui/react';
 import { AddIcon } from '@chakra-ui/icons';
+import AddPlace from './AddPlace';
 
 const MAPBOX_TOKEN =
   'pk.eyJ1IjoiYWRnLWJyYW5kaW5nIiwiYSI6ImNsM3czZ3IwZDBuaGYzYm8yemcwdWFlMGgifQ.2378CUUNBJppYXdD1c5aHg';
@@ -54,45 +56,48 @@ const ProjectMap = () => {
   };
 
   return (
-    <Map
-      {...viewport}
-      onMove={(event) => setViewport(event.viewState)}
-      style={{ width: '100%', height: '100%' }}
-      mapStyle='mapbox://styles/adg-branding/cl47jmywy003p15rmjzucu62i'
-      mapboxAccessToken={MAPBOX_TOKEN}
-    >
-      <GeoCode position='top-left' mapboxAccessToken={MAPBOX_TOKEN} />
-      {marker ? (
-        <Popup
-          longitude={marker.center[0]}
-          latitude={marker.center[1]}
-          onClose={() => dispatch(setPin(null))}
-        >
-          <Container>
-            <Box py={'2'}>
-              <Text fontSize={'medium'} lineHeight={'5'}>
-                {marker.place_name}
-              </Text>
-            </Box>
-            <Box py={'2'}>
-              <HStack spacing={'4'}>
-                <Tag
-                  size={'md'}
-                  key={'md'}
-                  variant='subtle'
-                  colorScheme='cyan'
-                  py={'2'}
-                  px={'4'}
-                >
-                  <TagLeftIcon boxSize='12px' as={AddIcon} />
-                  <TagLabel>Add to Map</TagLabel>
-                </Tag>
-              </HStack>
-            </Box>
-          </Container>
-        </Popup>
-      ) : null}
-    </Map>
+    <>
+      <AddPlace />
+      <Map
+        {...viewport}
+        onMove={(event) => setViewport(event.viewState)}
+        style={{ width: '100%', height: '100%' }}
+        mapStyle='mapbox://styles/adg-branding/cl47jmywy003p15rmjzucu62i'
+        mapboxAccessToken={MAPBOX_TOKEN}
+      >
+        <GeoCode position='top-left' mapboxAccessToken={MAPBOX_TOKEN} />
+        {marker ? (
+          <Popup
+            longitude={marker.center[0]}
+            latitude={marker.center[1]}
+            onClose={() => dispatch(setPin(null))}
+          >
+            <Container>
+              <Box py={'2'}>
+                <Text fontSize={'medium'} lineHeight={'5'}>
+                  {marker.place_name}
+                </Text>
+              </Box>
+              <Box py={'2'}>
+                <HStack spacing={'4'}>
+                  <Tag
+                    size={'md'}
+                    key={'md'}
+                    variant='subtle'
+                    colorScheme='cyan'
+                    py={'2'}
+                    px={'4'}
+                  >
+                    <TagLeftIcon boxSize='12px' as={AddIcon} />
+                    <TagLabel>Add to Map</TagLabel>
+                  </Tag>
+                </HStack>
+              </Box>
+            </Container>
+          </Popup>
+        ) : null}
+      </Map>
+    </>
   );
 };
 

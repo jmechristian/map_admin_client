d48f028020841a03aebab07bfa0b85a04530bffe
jmechristian <jmechristian@gmail.com>
2022-09-30 17:31:57 -0400

    push

--- components/EditModal.js
+++ components/EditModal.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useSelector } from 'react-redux';
 import {
+  Box,
   Modal,
   ModalOverlay,
   ModalContent,
@@ -14,6 +15,10 @@ import {
   FormLabel,
   Input,
   Textarea,
+  Select,
+  Flex,
+  Checkbox,
+  CheckboxGroup,
 } from '@chakra-ui/react';
 
 const EditModal = ({
@@ -33,11 +38,24 @@ const EditModal = ({
   const [projectName, setProjectName] = useState('');
   const [projectDesc, setProjectDesc] = useState('');
   const [projLink, setProjLink] = useState('');
+  const [structure, setStructureType] = useState();
+  const [collaborators, setCollaborators] = useState();
+  const [size, setSize] = useState('');
+
+  const projectTypes = [
+    'New Construction',
+    'Renovation',
+    'Historic Renovation',
+    'Addition',
+  ];
 
   useEffect(() => {
     setProjectName(selectedPin.attributes.name);
     setProjectDesc(selectedPin.attributes.description);
     setProjLink(selectedPin.attributes.link);
+    setStructureType(selectedPin.attributes.building_type.data.id);
+    setCollaborators(selectedPin.attributes.collaborators);
+    setSize(selectedPin.attributes.size);
   }, [selectedPin]);
 
   const editSubmitHandler = async (e) => {
@@ -48,6 +66,9 @@ const EditModal = ({
         name: projectName,
         description: projectDesc,
         link: projLink,
+        building_type: structure,
+        size: size,
+        collaborators: collaborators,
       },
     };
 
@@ -101,15 +122,61 @@ const EditModal = ({
                 onChange={(e) => setProjectDesc(e.target.value)}
               />
             </FormControl>
-            <FormControl mt={4}>
-              <FormLabel>Project Link</FormLabel>
+            <Box my={2}>
+              <FormLabel htmlFor='name'>Size</FormLabel>
               <Input
-                placeholder='Project Link'
+                id='size'
+                placeholder='Project Size'
+                type='text'
                 borderColor={'gray.300'}
-                value={projLink}
-                onChange={(e) => setProjLink(e.target.value)}
+                value={size ? size : ''}
+                onChange={(e) => setSize(e.target.value)}
+                focusBorderColor='brand.900'
               />
-            </FormControl>
+            </Box>
+            <Box pt={2}>
+              <FormLabel htmlFor='name'>Collaborators</FormLabel>
+              <Textarea
+                placeholder='Add optional project collaborators'
+                onChange={(e) => setCollaborators(e.target.value)}
+                borderColor={'gray.300'}
+                focusBorderColor='brand.900'
+                noOfLines={'2'}
+                value={collaborators}
+              />
+            </Box>
+            <Flex direction={'row'} mt='4' gap={6}>
+              <Box width={'50%'}>
+                <FormLabel htmlFor='dept'>Structure Type</FormLabel>
+                <Select
+                  placeholder='Select Structure Type'
+                  id='structure'
+                  onChange={(e) => setStructureType(e.target.value)}
+                  value={structure}
+                  color='gray.500'
+                  borderColor={'gray.300'}
+                  focusBorderColor='brand.900'
+                >
+                  <option value='1'>Condo</option>
+                  <option value='2'>Apartment</option>
+                  <option value='3'>Hotel</option>
+                  <option value='4'>Restaurant</option>
+                  <option value='5'>Rowhome</option>
+                  <option value='6'>House</option>
+                </Select>
+              </Box>
+              <Box width={'50%'}>
+                <FormControl>
+                  <FormLabel>Project Link</FormLabel>
+                  <Input
+                    placeholder='Project Link'
+                    borderColor={'gray.300'}
+                    value={projLink}
+                    onChange={(e) => setProjLink(e.target.value)}
+                  />
+                </FormControl>
+              </Box>
+            </Flex>
           </ModalBody>
 
           <ModalFooter>
